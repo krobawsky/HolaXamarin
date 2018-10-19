ca0b37f11d3eb65601cd5d9e33f7df79c1a8ac73
krobawsky <richibq20@gmail.com>
2018-10-19 13:41:48 -0500

    first commit

---
+++ MiPrimeraApp/MiPrimeraApp/Empleado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiPrimeraApp
+{
+    public class Empleado
+    {
+        public string nombre { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ MiPrimeraApp/MiPrimeraApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MiPrimeraApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public Empleado  NuevEmpleado { get; set; }
+
+        public MainPage()
+        {
+            var label = new Label
+            {
+                Text = "Ingresa tus datos"
+            };
+
+            var txtNombre = new Entry
+            {
+                Placeholder = "Escribre tu name"
+            };
+
+            var txtEmail = new Entry
+            {
+                Placeholder = "Escribre tu email"
+            };
+
+            var button = new Button
+            {
+                Text = "Apachurrale"
+            };
+
+            button.Clicked += (sender, e) =>
+            {
+                DisplayAlert("Mensaje", txtNombre.Text, "Ok");
+                NuevEmpleado = new Empleado();
+                NuevEmpleado.nombre = txtNombre.Text;
+                NuevEmpleado.email = txtEmail.Text;
+                BindingContext = NuevEmpleado;
+                InitializeComponent();
+            };
+
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = {label, txtNombre, txtEmail, button }
+            };
+
+        }
+    }
+}
